207e27d9cb10d3075154c5c15661c2c63631e6fd
Bijay Thakur <bijaythakur330@gmail.com>
2022-08-26 16:25:14 +0545

    loading addtional tags

--- index.php
+++ index.php
@@ -3,18 +3,13 @@
 
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <style>
-    :lang(ar) {
-      direction: rtl;
-    }
-  </style>
+  <?php wp_head(); ?>
 </head>
 
 <body>
   <p>Hello World!</p>
+
+  <?php wp_footer(); ?>
 </body>
 
 </html>
